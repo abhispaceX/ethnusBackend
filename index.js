fa133465e5667dd4832b7440a799299e09aad727
abhispaceX <141249160+abhispaceX@users.noreply.github.com>
2024-07-19 13:20:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000', // or whatever your frontend URL is
+  origin: 'https://ethnusbackend-fuxl.onrender.com', // or whatever your frontend URL is
   credentials: true
 }))
 app.use(express.json());
